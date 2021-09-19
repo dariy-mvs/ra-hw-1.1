3ccd210332c525f3625204f3eb6849ed4aa339a8
Marina Dariy <dariy.mvs@gmail.com>
2021-09-19 18:31:24 +0300

    initial commit

---
+++ src/ShopItemFunc.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './css/main.css';
+
+export default function ShopItemFunc(props) {
+  const itemInfo = props.item;
+  const {brand, title, description, descriptionFull, price, currency } = itemInfo;
+  console.log(itemInfo);
+  return (
+    <div>
+      <div className="main-content">
+        <h2>{brand}</h2>
+        <h1>{title}</h1>
+        <h3>{description}</h3>
+        <div className="description">{descriptionFull}</div>
+        <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+          <div className="price">{currency}, {price}</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
